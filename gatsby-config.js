0b07b5e4f9f16122af421e175711a3ddaa119413
Kerk Zhan <kerkzhan@gmail.com>
2021-03-02 18:32:09 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gatsby Tailwind`,
+    description: `Tailwind V2 with Gatsby`,
+    author: `Boon Kerk Zhan`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    },
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet'
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
-import React from "react"
+import React from "react";
 
-export default function Home() {
-  return <div>Hello world!</div>
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+        title="Home"
+      />
+
+      <section className="text-center">
+
+        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
+          Hey there! Welcome to your first Gatsby site.
+        </h2>
+
+        <p className="leading-loose">
+          This is a barebones starter for Gatsby styled using{` `}
+          <a
+            className="font-bold text-gray-900 no-underline"
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind CSS
+          </a>
+          , a utility-first CSS framework.
+        </p>
+      </section>
+    </Layout>
+  );
 }
+
+export default IndexPage;
